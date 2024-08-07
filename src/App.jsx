e6fe98afb63e54a576803b85f65d361834660fab
midhun04 <midhundas3110@gmail.com>
2024-08-08 00:47:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header/Header.jsx'
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examles from './components/Examples.jsx';
+function App() {
+  return (
+   <>
+      <Header/>
+      <main>
+        
+        <CoreConcepts/>
+        <Examles/>
+          
+      </main>
+    </>
+  );
+}
+export default App;
